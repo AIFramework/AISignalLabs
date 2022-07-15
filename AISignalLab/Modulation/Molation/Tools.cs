b756f67667c1e037e375b51198273af7f3996e6c
Zahar A. Ponimash <ponimashz@mail.ru>
2022-07-16 02:56:20 +0300

    Начата работа над модуляциями

---
+++ AISignalLab/Modulation/Molation/Tools.cs
@@ -0,0 +1,85 @@
+﻿using AI.DataStructs.Algebraic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.SignalLab.Modulation.Molation
+{
+    public class ModulationBitsTools
+    {
+        /// <summary>
+        /// Перевод битов в модулирующий сигнал
+        /// </summary>
+        /// <param name="bools">Биты</param>
+        /// <param name="bitDuration">Длительность бита</param>
+        /// <param name="lowSignal">Нижний уровень</param>
+        /// <param name="hSignal">Верхний уровень</param>
+        /// <param name="sr">Частота дискретизации</param>
+        public static Vector Bits2Signal(bool[] bools, double bitDuration = 3e-3, double lowSignal = 0, double hSignal = 1, double sr = 8e+3) 
+        {
+            int nSimb = (int)(bitDuration * sr); // Отсчетов на символ
+            Vector bitsSignal = new Vector(nSimb*bools.Length);
+            
+            for (int i = 0; i < bools.Length; i++)
+            {
+                if (bools[i])
+                {
+                    // Установка высокого уровня
+                    int end = (i + 1) * nSimb;
+                    for (int j = i*nSimb; j < end; j++)
+                        bitsSignal[j] = hSignal;
+                }
+                else 
+                {
+                    // Установка низкого уровня
+                    int end = (i + 1) * nSimb;
+                    for (int j = i * nSimb; j < end; j++)
+                        bitsSignal[j] = lowSignal;
+                }
+
+            }
+
+            return bitsSignal;
+        }
+
+        /// <summary>
+        /// Перевод цифрового объекта в модулирующий сигнал
+        /// </summary>
+        /// <param name="bools">Биты</param>
+        /// <param name="bitDuration">Длительность бита</param>
+        /// <param name="lowSignal">Нижний уровень</param>
+        /// <param name="hSignal">Верхний уровень</param>
+        /// <param name="sr">Частота дискретизации</param>
+        public static Vector Object2Signal(object obj, double bitDuration = 3e-3, double lowSignal = 0, double hSignal = 1, double sr = 8e+3)
+        {
+            BitArray bits = new BitArray(Obj2Bytes(obj));
+            bool[] bools = new bool[bits.Length];
+            
+            for (int i = 0; i < bits.Length; i++)
+                bools[i] = bits[i];
+
+            return Bits2Signal(bools, bitDuration, lowSignal, hSignal, sr);
+        }
+
+        /// <summary>
+        /// Конвертирование из obj в массив байт
+        /// URL: https://stackoverflow.com/questions/4865104/convert-any-object-to-a-byte
+        /// </summary>
+        static byte[] Obj2Bytes(object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ ModulationTest/Form1.cs
@@ -0,0 +1,38 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.SignalLab.Modulation.Molation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModulationTest
+{
+    public partial class Form1 : Form
+    {
+        double sr = 144.1e+3;// Частота дискретизации
+        double f0 = 9e+3;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vector signal = ModulationBitsTools.Object2Signal(textBox1.Text, bitDuration:0.0003, sr:sr);
+            Vector ask = new Vector(signal.Count);
+            for (int i = 0; i < signal.Count; i++)
+                ask[i] = signal[i] * Math.Cos((2 * Math.PI * f0 * i) / sr);
+
+            chartVisual1.PlotBlack(ask);
+        }
+    }
+}
