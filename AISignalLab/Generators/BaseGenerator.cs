b836ddcff576a0881df26622cc7b0dfb4c2c19bd
Zahar A. Ponimash <ponimashz@mail.ru>
2022-07-15 03:58:33 +0300

    AGC

--- AISignalLab/Generators/BaseGenerator.cs
+++ AISignalLab/Generators/BaseGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AI.Statistics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,43 @@ namespace AI.SignalLab.Generators
     /// </summary>
     public class BaseGenerator : IDisposable
     {
+        /// <summary>
+        /// Период через который обновлять сигнал
+        /// </summary>
         public int PeriodMS { get; set; } = 20;
+        /// <summary>
+        /// Функция генерации сигнала
+        /// </summary>
         public Func<int, double> Signal = (i) => i / 1000.0;
-        
+        /// <summary>
+        /// Коэф. аддитивного шума
+        /// </summary>
+        public double AdditivNoiseKoef { get; set; } = 0;
+        /// <summary>
+        /// Коэф. мультипликативного шума
+        /// </summary>
+        public double MultiplNoiseKoef { get; set; } = 0;
+        /// <summary>
+        /// Коэф. аддитивного тренда
+        /// </summary>
+        public double AdditivTrend { get; set; } = 0;
+        /// <summary>
+        /// Коэф. мультипликативного тренда
+        /// </summary>
+        public double MultiplTrend { get; set; } = 0;
+        /// <summary>
+        /// Генератор аддитивного шума
+        /// </summary>
+        public Func<Random, double> NoiseAdditivGenerator { get; set; } = Statistic.Gauss;
+        /// <summary>
+        /// Генератор мультипликативного шума
+        /// </summary>
+        public Func<Random, double> NoiseMultiplGenerator { get; set; } = Statistic.Gauss;
+
+
         int _counter = 0;
         Thread _thread;
+        Random _random = new Random();
 
         public void SetPosition(int start) 
         {
@@ -43,7 +76,14 @@ namespace AI.SignalLab.Generators
         {
             while (true)
             {
-                double s = Signal(_counter++);
+                double t = _counter / 1000.0;
+                double s = Signal(_counter)*(MultiplNoiseKoef* NoiseMultiplGenerator(_random)+1);
+                s += AdditivNoiseKoef * NoiseAdditivGenerator(_random);
+                s *= MultiplTrend * t + 1;
+                s += AdditivTrend * t;
+                _counter++;
+
+
                 SignalSemple(s);
                 Thread.Sleep(PeriodMS);
             }
